afb45b856ec10df75bf56fd95c6de6a063001fe4
Juwuan Turner-Howard <juwuanturnerhoward@gmail.com>
2021-03-18 19:39:49 -0500

    TSK-43 Panel manager interface. (#2)

--- src/main/java/coursescheduler/CourseScheduler.java
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -1,6 +1,9 @@
 package coursescheduler;
 
-/** Entry-point for Course Scheduler desktop application. */
+/**
+ * Entry-point for Course Scheduler desktop application.
+ */
 final class CourseScheduler {
-  public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 }
