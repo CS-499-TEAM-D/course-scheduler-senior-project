8e815b93c40ca69ea79402ed009f5541ce80ea2b
Juwuan Turner-Howard <juwuanturnerhoward@gmail.com>
2021-03-18 19:39:49 -0500

    TSK-43 Panel manager interface. (#2)

---
+++ src/main/java/coursescheduler/managers/PanelManager.java
@@ -0,0 +1,20 @@
+/**
+ * @author(s) juwuanturnerhoward
+ */
+
+package coursescheduler.managers;
+
+import javax.swing.JComponent;
+
+/**
+ * Handles the updating the page inside of a container and state of main container.
+ *
+ * @param <C> element to display the page.
+ * @param <P> element to show as page.
+ */
+abstract class PanelManager<C, P extends JComponent> implements PanelController<P> {
+    @Override
+    public void update(P page) {
+        // TODO(jth): Implement.
+    }
+}
