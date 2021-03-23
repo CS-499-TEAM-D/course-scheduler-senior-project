b54d01b379fac07466bdfbbf70605a71a7e8aa7d
theChrisODOM <csodom22@gmail.com>
2021-03-22 20:19:49 -0500

    Added Google Sheets API Utility
    Added the ability to read and write to a remote google sheet.

---
+++ src/main/java/DatabaseModule/SheetsServiceUtil.java
@@ -0,0 +1,25 @@
+package DatabaseModule;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class SheetsServiceUtil {
+    private static final String APPLICATION_NAME = "Course Scheduler";
+
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        Credential credential = GoogleAuthUtil.authorize(HTTP_TRANSPORT);
+        return new Sheets.Builder(
+                HTTP_TRANSPORT,
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+}
