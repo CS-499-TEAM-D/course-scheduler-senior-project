f5216bba84595c37e6d18e5e4c133fbfe6dbc4d2
Deoxys0 <evilcreature123@gmail.com>
2021-03-24 11:58:15 -0500

    Rebuilt debugger; doesn't currently use PanelManager (but it's included anyway)

---
+++ src/main/java/coursescheduler/CourseScheduler.java
@@ -0,0 +1,13 @@
+package coursescheduler;
+
+/** Entry-point for Course Scheduler desktop application. */
+final class CourseScheduler {
+  public static void main(String[] args) 
+  {
+      debuggerControl control = new debuggerControl();
+      debuggerFrame debugger = new debuggerFrame();
+      debugger.setVisible(true);
+      
+      
+  }
+}
---
+++ src/main/java/coursescheduler/debuggerControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursescheduler;
+
+/**
+ *
+ * @author evilc
+ */
+public class debuggerControl 
+{
+    debuggerFrame frame;
+    public debuggerControl()
+    {
+        /*
+        frame = new debuggerFrame();
+        LoadSchedulePage page1 = new LoadSchedulePage();
+        SaveSchedulePage page2 = new SaveSchedulePage();
+        frame.addPage(page1, "LOAD_SCHEDULE");
+        frame.addPage(page2, "SAVE_SCHEDULE");
+        */
+    }
+
+}
