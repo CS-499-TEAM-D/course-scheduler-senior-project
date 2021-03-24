05e765795cea5525a661e8adabaf0edf3a4b00c6
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-03-23 22:17:24 -0500

    TSK-56 Gradle test folder

---
+++ src/test/java/coursescheduler/CourseSchedulerTest.java
@@ -0,0 +1,12 @@
+package coursescheduler;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CourseSchedulerTest {
+
+    @Before
+    public void setUp() {}
+}
