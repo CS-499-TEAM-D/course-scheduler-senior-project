0e750a0409349ae1aa26a5fc6841596ad349609e
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-04-17 17:38:47 -0500

    TSK-22 Merging in changes from develop

--- src/test/java/coursescheduler/CourseSchedulerTest.java
+++ src/test/java/coursescheduler/CourseSchedulerTest.java
@@ -1,25 +1,25 @@
 package coursescheduler;
 
-import com.google.inject.Guice;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
+import client.daos.BaseUserDao;
+import client.daos.UserDao;
 import org.junit.Test;
 
 import coursescheduler.managers.BaseFrameManager;
 import coursescheduler.managers.PanelController;
 import coursescheduler.security.BaseCredentialsVerifier;
 import coursescheduler.security.CredentialsVerifier;
+import coursescheduler.views.pages.AbstractPageFactory;
+import coursescheduler.views.pages.BaseAbstractPageFactory;
 import coursescheduler.views.pages.BaseLoginPage;
 
 public class CourseSchedulerTest {
-  private final Injector injector = Guice.createInjector(new CourseSchedulerModule());
-  private final CourseScheduler courseScheduler = injector.getInstance(CourseScheduler.class);
-
-//  private final CourseSchedulerFrame courseSchedulerFrame = new CourseSchedulerFrame();
-//  private final PanelController controller = new BaseFrameManager(courseSchedulerFrame);
-//  private final CredentialsVerifier verifier = new BaseCredentialsVerifier();
-//  private final BaseLoginPage loginPage = new BaseLoginPage(controller, verifier);
-//  private final CourseScheduler courseScheduler = new CourseScheduler(controller, loginPage);
+  private final CourseSchedulerFrame courseSchedulerFrame = new CourseSchedulerFrame();
+  private final PanelController controller = new BaseFrameManager(courseSchedulerFrame);
+  private final CredentialsVerifier verifier = new BaseCredentialsVerifier();
+  private final UserDao userDao = new BaseUserDao();
+  private final AbstractPageFactory factory = new BaseAbstractPageFactory(controller, verifier, userDao);
+  private final BaseLoginPage loginPage = new BaseLoginPage(controller, verifier, factory, userDao);
+  private final CourseScheduler courseScheduler = new BaseCourseScheduler(controller, loginPage);
 
   @Test
   public void main_defaultState_shouldNotFail() {
