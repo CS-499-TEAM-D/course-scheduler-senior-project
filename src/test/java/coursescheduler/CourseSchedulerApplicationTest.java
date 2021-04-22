dab4db25c6c8e5ebe5a163cca234e4520d2e2aa4
juwuanturnerhow <juwuanturnerhoward@gmail.com>
2021-04-22 17:24:09 -0500

    SPR-02 Update CourseSchedulerApplication test

--- src/test/java/coursescheduler/CourseSchedulerApplicationTest.java
+++ src/test/java/coursescheduler/CourseSchedulerApplicationTest.java
@@ -1,9 +1,9 @@
 package coursescheduler;
 
-import coursescheduler.client.daos.BaseUserDao;
-import coursescheduler.client.daos.UserDao;
 import org.junit.Test;
 
+import coursescheduler.client.daos.BaseUserDao;
+import coursescheduler.client.daos.UserDao;
 import coursescheduler.managers.BaseFrameManager;
 import coursescheduler.managers.PanelController;
 import coursescheduler.security.BaseCredentialsVerifier;
@@ -11,15 +11,18 @@ import coursescheduler.security.CredentialsVerifier;
 import coursescheduler.views.pages.AbstractPageFactory;
 import coursescheduler.views.pages.BaseAbstractPageFactory;
 import coursescheduler.views.pages.BaseLoginPage;
+import coursescheduler.views.pages.Page;
 
 public class CourseSchedulerApplicationTest {
   private final CourseSchedulerFrame courseSchedulerFrame = new CourseSchedulerFrame();
   private final PanelController controller = new BaseFrameManager(courseSchedulerFrame);
   private final CredentialsVerifier verifier = new BaseCredentialsVerifier();
   private final UserDao userDao = new BaseUserDao();
-  private final AbstractPageFactory factory = new BaseAbstractPageFactory(controller, verifier, userDao);
-  private final BaseLoginPage loginPage = new BaseLoginPage(controller, verifier, factory, userDao);
-  private final CourseSchedulerApplication courseSchedulerApplication = new CourseScheduler(controller, loginPage);
+  private final AbstractPageFactory factory =
+      new BaseAbstractPageFactory(controller, verifier, userDao);
+  private final Page loginPage = new BaseLoginPage(controller, verifier, factory, userDao);
+  private final CourseSchedulerApplication courseSchedulerApplication =
+      new CourseScheduler(controller, loginPage);
 
   @Test
   public void main_defaultState_shouldNotFail() {
