1b1d5f0fd36c750fae85e4dbd03d23fae1b5cd0a
theChrisODOM <csodom22@gmail.com>
2021-04-25 17:24:32 -0500

    set up dependencies for google sheets api

--- src/main/java/coursescheduler/security/BaseCredentialsVerifier.java
+++ src/main/java/coursescheduler/security/BaseCredentialsVerifier.java
@@ -2,8 +2,10 @@ package coursescheduler.security;
 
 /** {@inheritDoc} */
 public class BaseCredentialsVerifier implements CredentialsVerifier {
+
   @Override
   public boolean validUserCredentials(String email, char[] password) {
-    return true;
+
+    return false;
   }
 }
