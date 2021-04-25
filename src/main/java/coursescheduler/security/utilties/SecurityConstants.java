ddbc7aee2b9311705c4c6a8dad1cc5c7d33d8c91
theChrisODOM <csodom22@gmail.com>
2021-04-25 17:48:08 -0500

    Added GoogleSheets Service
    GoogleAuthUtil builds a credential objected needed for OAuth2.0
    
    SheetsService builds a service instance used to communicate with the API
    
    SecurityConstants contains String constants needed for navigating the remote sheet database

---
+++ src/main/java/coursescheduler/security/utilties/SecurityConstants.java
@@ -0,0 +1,16 @@
+package coursescheduler.security.utilties;
+
+public final class SecurityConstants {
+
+    private SecurityConstants() {
+    }
+
+    public static final String CREDENTIALS_SHEET = "CREDS";
+    public static final String USERNAME_COLUMN = "A";
+    public static final String PASSWORD_COLUMN = "B";
+    public static final String ROLE_COLUMN = "C";
+    public static final String PASSWORD_UPLOAD_COLUMN = "D";
+    public static final String VERIFICATION_COLUMN = "E";
+    public static final String DEPARTMENT_COLUMN = "F";
+
+}
