dd765f17026a50ad5908070806a5e64fea3a51e6
__chrisOdom <47047900+theChrisODOM@users.noreply.github.com>
2021-04-26 13:42:36 -0500

    Tsk 88 security (#16)
    * set up dependencies for google sheets api
    
    * Added GoogleSheets Service
    
    GoogleAuthUtil builds a credential objected needed for OAuth2.0
    
    SheetsService builds a service instance used to communicate with the API
    
    SecurityConstants contains String constants needed for navigating the remote sheet database
    
    * updated verifier, added constants
    
    verifier now contains logic with sheets api to verify users against their credentials stored in the remote sheet
    
    constants contains String constants needed for the verifier to work

--- src/main/java/coursescheduler/security/BaseCredentialsVerifier.java
+++ src/main/java/coursescheduler/security/BaseCredentialsVerifier.java
@@ -1,9 +1,81 @@
 package coursescheduler.security;
 
-/** {@inheritDoc} */
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import coursescheduler.security.utilties.SheetsService;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static coursescheduler.security.utilties.SecurityConstants.*;
+
+/**
+ * {@inheritDoc}
+ */
 public class BaseCredentialsVerifier implements CredentialsVerifier {
-  @Override
-  public boolean validUserCredentials(String email, char[] password) {
-    return true;
-  }
+
+    @Override
+    public boolean validUserCredentials(String email, char[] password) {
+        try {
+            Sheets service = SheetsService.getSheetsService();
+            int userIndex = findIndexOfStringInSheetColumn(service, CREDENTIALS_SPREADSHEET, CREDENTIALS_SHEET, USERNAME_COLUMN, email);
+
+            ValueRange uploadPasswordValueRange = new ValueRange().setValues(Arrays.asList(
+                    Arrays.asList(String.valueOf(password))
+            ));
+            // write the password attempt to the remote sheet
+            service.spreadsheets().values().update(
+                    CREDENTIALS_SPREADSHEET,
+                    CREDENTIALS_SHEET + PASSWORD_UPLOAD_COLUMN + userIndex,
+                    uploadPasswordValueRange)
+                    .setValueInputOption("RAW")
+                    .execute();
+            // fetch from the verification column either true if the password attempt = password saved or false if not
+            boolean verified = Boolean.parseBoolean(service.spreadsheets().values().get(
+                    CREDENTIALS_SPREADSHEET,
+                    CREDENTIALS_SHEET+VERIFICATION_COLUMN+userIndex)
+                    .execute()
+                    .getValues()
+                    .get(0).get(0)
+                    .toString()
+            );
+            // clear the password attempt column on the remote sheet
+            uploadPasswordValueRange.getValues().get(0).set(0, "");
+            service.spreadsheets().values().update(
+                    CREDENTIALS_SPREADSHEET,
+                    CREDENTIALS_SHEET + PASSWORD_UPLOAD_COLUMN + userIndex,
+                    uploadPasswordValueRange)
+                    .setValueInputOption("RAW")
+                    .execute();
+            return verified;
+
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private int findIndexOfStringInSheetColumn(Sheets service, String spreadsheetID, String sheetTitle, String column, String stringToFind) {
+        try {
+            List<List<Object>> stringsInColumn = service.spreadsheets().values().get(
+                    spreadsheetID,
+                    sheetTitle+column)
+                    .execute()
+                    .getValues();
+            int index = 0;
+            for (List string : stringsInColumn){
+                if(string.get(0).toString().equals(stringToFind)){
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
 }
---
+++ src/main/java/coursescheduler/security/utilties/SheetsService.java
@@ -0,0 +1,34 @@
+package coursescheduler.security.utilties;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+
+
+public class SheetsService {
+
+    private static final String APPLICATION_NAME = "Course Scheduler";
+
+    private static Sheets service = null;
+
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        if (service != null) {
+            return service;
+        }
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Credential credential = GoogleAuthUtil.authorize(HTTP_TRANSPORT);
+        return service = new Sheets.Builder(
+                HTTP_TRANSPORT,
+                JacksonFactory.getDefaultInstance(), credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+
+}
